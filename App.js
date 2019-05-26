a7092995f82722ca8e13d55fdbc5f540c671a441
Edwin Capel <edwinrc1026@gmail.com>
2019-05-27 01:12:31 +0800

    hardcoded ui without data api

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, KeyboardAvoidingView , Platform} from 'react-native';
+
+import SearchInput from './components/SearchInput'
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Hello Edwin !</Text>
-        <Text>HOT RELOAD </Text>
-      </View>
+      <KeyboardAvoidingView style={styles.container} behavior="padding">
+        <Text style={[styles.largeText, styles.textStyle, styles.blue]}>San Francisco</Text>
+        <Text style={[styles.smallText, styles.textStyle]}>Light Cloud</Text>
+        <Text style={[styles.largeText, styles.textStyle]}>24°</Text>
+
+        <SearchInput placeholder="Search any city"/>
+      </KeyboardAvoidingView>
     );
   }
 }
@@ -19,4 +24,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  blue: {
+    color: 'blue'
+  },
+  textStyle: {
+    textAlign: 'center',
+    fontFamily: Platform.OS == 'ios' ? 'AvenirNext-Regular' : 'Roboto'
+  },
+  largeText: {
+    fontSize: 44
+  },
+  smallText: {
+    fontSize: 18
+  }
 });
+
